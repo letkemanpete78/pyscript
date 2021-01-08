ffc172e7682d45e7db3fda87239d48259add4e65
peteletkeman78 <letkemanpete@gmail.com>
2021-01-07 20:56:36 -0500

    rafctoring

--- directory_cleanUp.py
+++ directory_cleanUp.py
@@ -8,21 +8,23 @@ import glob
 def handle_dups(path,filenames):
     filenames.sort(key=trim_filename)
     duppath = os.path.join(path,"dups")
-    if not os.path.exists(duppath):
-        os.mkdir(duppath)
-    for filename in filenames:
-        f = replace('the ','*',filename.replace('.','*').replace(' ','*')) + '*'
-        file_subset = glob.glob(os.path.join(path, f))
-        if len(file_subset) > 1:
-            dupfiles = []
-            for name in file_subset:
-                dupfiles.append({'name':os.path.basename(name), 'size':os.stat(name).st_size})
-            dupfiles = sorted(dupfiles, key=lambda x: x['size'],reverse=True)[1:]
-            for f in dupfiles:
-                dst = os.path.join(path,duppath,f['name'])
-                src = os.path.join(path,f['name'])
-                os.rename(src,dst)
-                print('moving dup: ',src,'->',dst)
+    with open('dups.txt', 'w') as duplog:
+        if not os.path.exists(duppath):
+            os.mkdir(duppath)
+            duplog.write('Created directory:',duppath)
+        for filename in filenames:
+            f = replace('the ','*',filename.replace('.','*').replace(' ','*')) + '*'
+            file_subset = glob.glob(os.path.join(path, f))
+            if len(file_subset) > 1:
+                dupfiles = []
+                for name in file_subset:
+                    dupfiles.append({'name':os.path.basename(name), 'size':os.stat(name).st_size})
+                dupfiles = sorted(dupfiles, key=lambda x: x['size'],reverse=True)[1:]
+                for f in dupfiles:
+                    dst = os.path.join(path,duppath,f['name'])
+                    src = os.path.join(path,f['name'])
+                    os.rename(src,dst)
+                    duplog.write('moving dup:',src,'->',dst ,"\n")
 
 def replace(old, new, str, caseinsentive = False):
     if caseinsentive:
@@ -40,47 +42,56 @@ def trim_filename(name):
 def rename_files(path):
     filenames = []
     otherpath = os.path.join(path,"Other")
-    if not os.path.exists(otherpath):
-        os.mkdir(otherpath)
-    with os.scandir(path) as entries:
-        for entry in entries:
-            if not os.path.isdir(os.path.join(path,entry.name)):
-                if "(j)" not in entry.name.lower():
-                    fname = entry.name
-                    newname = re.sub('\([a-zA-Z!@#$%^&*\[\]]*\)','',fname).lower().replace(' .','.').title().replace('(','').replace(')','').replace(',','.').replace('-',' ').replace('_',' ')
-                    firstdigit = True
-                    finalname = ""
-                    for c in newname:
-                        if c.isdigit():
-                            if firstdigit:
-                                finalname += " " + c
-                                firstdigit = False
-                            else:
-                                finalname += c
-                                
+
+    with open('cleanup.txt', 'w') as cleanlog:
+        if not os.path.exists(otherpath):
+            os.mkdir(otherpath)
+            cleanlog.write('Created directory',otherpath,"\n")
+        with os.scandir(path) as entries:
+            for entry in entries:
+                if not os.path.isdir(os.path.join(path,entry.name)):
+                    if "(j)" not in entry.name.lower():
+                        fname = entry.name
+                        newname = re.sub('\([a-zA-Z!@#$%^&*\[\]]*\)','',fname).lower().replace(' .','.').title().replace('(','').replace(')','').replace(',','.').replace('-',' ').replace('_',' ')
+                        finalname = space_before_num(newname)
+                        if ' the.' in fname.lower():
+                            finalname = 'The ' + replace('the.','',finalname,True)
+                        finalname = finalname.replace("  "," ").strip()
+                        if finalname != fname:
+                            filenames.append(try_rename(path, cleanlog, fname, finalname))
                         else:
-                            finalname += c
-                            firstdigit = True
-                    if ' The.' in fname:
-                        finalname = 'The ' + finalname.replace(' The.','')
-                    finalname = finalname.replace("  "," ").strip()
-                    if finalname != fname:
-                        if not os.path.exists(path + finalname):
-                            print("renaming: " + fname + " -> " + finalname)
-                            os.rename(path + fname,path + finalname)
-                            filenames.append(trim_filename(finalname))
+                            completedlog.write('done:',fname,"\n")
+                            filenames.append(trim_filename(fname))
+                    else:
+                        if not os.path.exists(os.path.join(otherpath,entry.name)):
+                            movelog.write('moving:',os.path.join(path,entry.name,"->",os.path.join(otherpath,entry.name)),"\n")
+                            os.rename(os.path.join(path,entry.name), os.path.join(otherpath,entry.name))
                         else:
-                            print('problem: ' + fname +' ->  ' + finalname)
-                    else:
-                        print('done: ' + fname)
-                        filenames.append(trim_filename(fname))
-                else:
-                    if not os.path.exists(os.path.join(otherpath,entry.name)):
-                        print('moving: ' + os.path.join(path,entry.name + "-> " + os.path.join(otherpath,entry.name)))
-                        os.rename(os.path.join(path,entry.name), os.path.join(otherpath,entry.name))
-                    else:
-                        print('problem-other: ' + entry.name)
+                            problemlog.write('problem-other:',entry.name,"\n")
     return list(set(filenames))
+
+def space_before_num(newname):
+    firstdigit = True
+    for c in newname:
+        if c.isdigit():
+            if firstdigit:
+                finalname += " " + c
+                firstdigit = False
+            else:
+                finalname += c
+        else:
+            finalname += c
+            firstdigit = True
+    return finalname
+
+def try_rename(path, cleanlog, fname, finalname):
+    if not os.path.exists(path + finalname):
+        cleanlog.write("renaming: " + fname + " -> " + finalname + "\n")
+        os.rename(path + fname,path + finalname)
+        return trim_filename(finalname)
+    else:
+        problemlog.write('problem:',fname,'->',finalname,"\n")
+        return ''
 
 def main():
     path = "/home/pete/Downloads/RomsCopy/test/"
